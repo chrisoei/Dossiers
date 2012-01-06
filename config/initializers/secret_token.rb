67981ff28f45a32338f45d876992884b9b9bde81
Chris Oei <chris@eluctari.com>
2012-01-06 12:24:34 -0800

    Added barebones rails 3.1.3 scaffold.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dossiers::Application.config.secret_token = '654c21a59d6b44978027c7e92dfa768d3dd7fc2c6e5f5d5fc1ddb468fb506335475aa983a5376589ab0cd4758c9ab2e3e07b1cf63929ee9c2b66e5002d6d502f'
