848f9772ad0bca368a759c96bb7f34dfea729e16
Chris Oei <chris@eluctari.com>
2012-01-08 14:14:45 -0800

    Fixed integration test. Use chrisoei user for test database.

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -1,12 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "the sign-in process" do
+  before(:each) do
+    user = User.new(:login=>'johndoe',:password=>'test123',:password_confirmation=>'test123')
+    user.save_without_session_maintenance
+  end
+  
+  it "should not let a user sign in with an incorrect password" do
+    visit login_path
+    fill_in 'Login', :with => 'johndoe'
+    fill_in 'Password', :with => 'test'
+    click_button 'Login'
+    current_path.should == user_session_path
+  end
+  
   it "should sign a user in" do
-    within('#session') do
-      fill_in 'Login', :with => 'chrisoei'
-      fill_in 'Password', :with => ''
-    end
-    click_link 'Sign in'   
+    visit login_path
+    fill_in 'Login', :with => 'johndoe'
+    fill_in 'Password', :with => 'test123'
+    click_button 'Login'
+    current_path.should == person_path
   end
 
 end
