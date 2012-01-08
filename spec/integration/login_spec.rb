27a2d66818f759852c3f77ce1f0d1a1c650d7194
Chris Oei <chris@eluctari.com>
2012-01-07 16:38:29 -0800

    Added Haml and login controller and route.

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "the sign-in process" do
-  it "should do something" do
+  it "should sign a user in" do
     within('#session') do
       fill_in 'Login', :with => 'chrisoei'
       fill_in 'Password', :with => ''
