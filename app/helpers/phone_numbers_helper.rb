0410095fbdb61829099e6fe504002c3217b70cb9
Chris Oei <chris@eluctari.com>
2012-01-10 14:31:50 -0800

    Added phone_format helper.

--- app/helpers/phone_numbers_helper.rb
+++ app/helpers/phone_numbers_helper.rb
@@ -1,2 +1,15 @@
 module PhoneNumbersHelper
+  def phone_format(p)
+    if p.start_with?('1')
+      if p.length == 11
+        "1 #{p[1..3]} #{p[4..6]}-#{p[7..10]}"
+      else
+        "1 #{p[1..3]} #{p[4..6]}-#{p[7..10]} X#{p[11..-1]}"
+      end
+    elsif p.start_with?('91')
+      "91 #{p[2..-1]}"
+    else
+      p
+    end
+  end
 end
