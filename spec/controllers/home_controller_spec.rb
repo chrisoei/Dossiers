b41e501aa73ed5f07166c76ceebcf2443037870e
Chris Oei <chris@eluctari.com>
2012-01-10 09:35:53 -0800

    More specific home controller spec.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe HomeController do
   it "should redirect to the people path" do
     get :index
-    response.should be_redirect
+    response.should redirect_to(people_path)
   end
 end
