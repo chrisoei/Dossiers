b88ca4c61a6d05056452a7750b566851b1b980fe
Chris Oei <chris@eluctari.com>
2012-01-10 16:59:31 -0800

    Added note helper so that (p#n) will display person #n and (o#n) will display organization #n.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def note_markup(s)
+    while s =~ /\(p#(\d+)\)/
+      pid = $1
+      s.sub!(/\(p#\d+\)/,"<a href='/people/#{pid}'>#{Person.find(pid).display_name}</a>")
+    end
+    while s =~ /\(o#(\d+)\)/
+      oid = $1
+      s.sub!(/\(o#\d+\)/,"<a href='/organizations/#{oid}'>#{Organization.find(oid).display_name}</a>")
+    end
+    s
+  end
 end
