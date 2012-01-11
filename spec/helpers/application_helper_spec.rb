e1d057780968eed8562ee9dac309e83c88ff7601
Chris Oei <chris@eluctari.com>
2012-01-11 15:51:04 -0800

    Facebook and Linkedin links are now actual links in show view.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  it "should mark up a note with an organization" do
+    
+  end
+  
+  it "should mark up a note with a person" do
+    pending
+  end
+end
