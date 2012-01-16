96a5796163dea4d060534100d0147acfbfea59a2
Chris Oei <chris@eluctari.com>
2012-01-15 22:30:33 -0800

    Use letter-spacing to spread phone numbers apart for easier reading.

--- app/helpers/phone_numbers_helper.rb
+++ app/helpers/phone_numbers_helper.rb
@@ -2,12 +2,12 @@ module PhoneNumbersHelper
   def phone_format(p)
     if p.start_with?('1')
       if p.length == 11
-        "1 #{p[1..3]} #{p[4..6]}-#{p[7..10]}"
+        "<span class='phone'>1 #{p[1..3]} #{p[4..6]}-#{p[7..10]}</span>".html_safe
       else
-        "1 #{p[1..3]} #{p[4..6]}-#{p[7..10]} X#{p[11..-1]}"
+        "<span class='phone'>1 #{p[1..3]} #{p[4..6]}-#{p[7..10]} X#{p[11..-1]}</span>".html_safe
       end
     elsif p.start_with?('91')
-      "91 #{p[2..-1]}"
+      "<span class='phone'>91 #{p[2..-1]}</span>".html_safe
     else
       p
     end
